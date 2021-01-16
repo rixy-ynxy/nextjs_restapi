2526150280258eb2512d6f2aa5cbf8ffa9ceefd3
Tokiwa_Ryuji <ryuji_tokiwa@yunixy.co.jp>
2021-01-16 20:42:27 +0900

    settings update 1

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -29,7 +29,7 @@ DEBUG = config('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOTS')
+ALLOWED_HOSTS = ['nextjs-api-heroku.herokuapp.com']
 
 
 # Application definition
