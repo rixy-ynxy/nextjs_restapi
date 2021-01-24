e2f96588f3f544b7144399d9ecd3d79b0bf1f647
Tokiwa_Ryuji <ryuji_tokiwa@yunixy.co.jp>
2021-01-24 14:22:20 +0900

    add cors white list

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
+    'http://localhost:3000',"https://nextjs-api-heroku.herokuapp.com/"
 ]
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
